a7ae0a64968fdf883e97ad4635775d4003165a81
unknown <ihsan.widagdo@gmail.com>
2024-01-17 18:53:24 +0700

    feat: create api for update and delete data

--- backend/app/Http/Controllers/DataController.php
+++ backend/app/Http/Controllers/DataController.php
@@ -28,6 +28,51 @@ class DataController extends Controller
         ]);
     }
 
+    public function updateData(Request $request, $id){
+        $data = $request->all();
+        $table = Account::where('id', $id)->first();
+
+        if(!$table){
+            return response()->json([
+                'message' => 'Data not found'
+            ], 404);
+        }
+
+        if($request->user()->id != $table->author_id) {
+            return response()->json([
+                'message' => 'Access Denied'
+            ], 403);
+        }
+        $table->update($data);
+        // dd($request->all());
+        $response = AccountResources::make($table);
+        return response()->json([
+            'data' => $response,
+            'message' => 'Data Updated Successfully'
+        ]);
+
+    }
+
+    public function deleteData(Request $request, $id){
+        $table = Account::where('id', $id)->first();
+
+        if(!$table){
+            return response()->json([
+                'message' => 'Data not found'
+            ], 404);
+        }
+
+        if($request->user()->id != $table->author_id) {
+            return response()->json([
+                'message' => 'Access Denied'
+            ], 403);
+        }
+        $table->delete();
+        return response()->json([
+            'message' => 'Data Deleted Successfully'
+        ]);
+    }
+
     public function showData(Request $request){
         $data = DB::table('accounts')->where('author_id', $request->user()->id)->get();
         $dataResource = AccountResources::collection($data);
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -21,6 +21,8 @@ Route::post('/login', [UserController::class, 'login']);
 
 Route::group(['middleware' => 'auth:sanctum'], function(){
     Route::get('/me', [UserController::class, 'getme']);
+    Route::patch('/data/{id}', [DataController::class, 'updateData'])->whereNumber('id');
+    Route::delete('/data/{id}', [DataController::class, 'deleteData'])->whereNumber('id');
     Route::post('/newdata', [DataController::class, 'addAccount']);
     Route::get('/data', [DataController::class, 'showData']);
     Route::get('/logout', [UserController::class, 'logout']);
